3039dfe9b949ad96ddcff9157117a388d5e6a8da
Johnny Briones <jbrrii91@gmail.com>
2020-09-12 22:00:28 -0400

    taking a break

--- main.js
+++ main.js
@@ -4,10 +4,38 @@ type;
 name;
 amount;
 
-document.getElementById("submitExpense").addEventListener("click", addExpense);
+document.getElementById("submitExpense").addEventListener("click", function(e) {
+        dateInput(e);
+        typeInput(e);
+        nameInput(e);
+        amountInput(e);
+    })
 
-function addExpense(e) {
-  console.log("It works!");
+function dateInput(e) {
+    const dateInput = document.getElementById('dateInput');
+date = document.createElement('tr');
+date.innerText = dateInput;
+date.id = 'expenseDate';
+date.appendChild(dateInput);
+  
+}
+
+function typeInput(e) {
+    const typeInput = document.getElementById('typeInput');
+
+  
+}
+
+function nameInput(e) {
+    const nameInput = document.getElementById('nameInput');
+
+  
+}
+
+function amountInput(e) {
+    const amountInput = document.getElementById('amountInput');
+
+  
 }
 
 function deleteExpense(e) {

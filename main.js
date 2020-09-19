26147b2c76fbd550214e9e7e2b0e86a20e29c49f
Johnny Briones <jbrrii91@gmail.com>
2020-09-19 01:51:11 -0400

    delete row

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-document.getElementById("submitExpense").addEventListener("click", function() {
-        makeTableRow();
-    })
-
-// document.getElementById('deleteButton').addEventListener("click", deleteExpense())
+document.getElementById("submitExpense").addEventListener("click", function () {
+  makeTableRow();
+});
 
 function deleteExpense(e) {
-  
+  document.getElementsByTagName("tr")[1].remove();
 }
 
 function makeTableRow() {
-    let dateUserInput = document.getElementById('dateInput').value;
-    const typeUserInput = document.getElementById('typeInput').value;
-    const amountUserInput = document.getElementById('amountInput').value;
-    const nameUserInput = document.getElementById('nameInput').value;
-    let tableRef = document.getElementById('expenseTable');
+  let dateUserInput = document.getElementById("dateInput").value;
+  const typeUserInput = document.getElementById("typeInput").value;
+  const amountUserInput = document.getElementById("amountInput").value;
+  const nameUserInput = document.getElementById("nameInput").value;
+  let tableRef = document.getElementById("expenseTable");
+  const inputs = document.querySelectorAll(".input");
 
-    let newRow = `
+  let newRow = `
         <tr>
             <td id='td1'>${dateUserInput}</td>
             <td id='td2'>${typeUserInput}</td>
             <td id='td3'>${nameUserInput}</td>
             <td id='td4'>${amountUserInput}</td>
-            <td> <input onclick='deleteExpense()' id= 'deleteButton' type= 'button' value='Delete'/></td>
+            <td> <input onclick='deleteExpense()' className = 'delete' id= 'deleteButton' type= 'button' value='Delete'/></td>
         </tr>
-        `
-    tableRef.innerHTML += newRow
-    const inputs = document.querySelectorAll('.input');
-    inputs.forEach(inputs => inputs.value = "");
-    }
+        `;
+
+  tableRef.innerHTML += newRow;
+  inputs.forEach((inputs) => (inputs.value = ""));
+}
+

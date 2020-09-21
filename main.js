87613cf18c5961779ece7c3beb3ad5c50e938721
Johnny Briones <jbrrii91@gmail.com>
2020-09-20 22:45:13 -0400

    it works

--- main.js
+++ main.js
@@ -2,28 +2,32 @@ document.getElementById("submitExpense").addEventListener("click", function () {
   makeTableRow();
 });
 
-
-function deleteExpense(event) {
-  let deleteBtn = document.getElementById('deleteButton');
-  let i = deleteBtn.parentNode.parentNode.rowIndex;
-  if (event.target.id == 'deleteButton'){
-    document.getElementById('expenseTable').deleteRow(i);
-    }
+function deleteExpense(e) {
+  if (e.target.id === "deleteButton") {
+    e.target.parentElement.parentElement.remove();
+  }
 }
 
 function makeTableRow() {
   let dateUserInput = document.getElementById("dateInput").value;
   const typeUserInput = document.getElementById("typeInput").value;
-  const amountUserInput = document.getElementById("amountInput").value;
+  let amountUserInput = document.getElementById("amountInput").value;
   const nameUserInput = document.getElementById("nameInput").value;
   let tableRef = document.getElementById("expenseTable");
   const inputs = document.querySelectorAll(".input");
 
+  let checkFor$ = amountUserInput.indexOf("$");
+  if (checkFor$ !== -1) {
+    amountUserInput = amountUserInput;
+  } else {
+    amountUserInput = "$" + amountUserInput;
+  }
+
   let newRow = `
         <tr>
             <td id='td1'>${dateUserInput}</td>
-            <td id='td2'>${typeUserInput}</td>
             <td id='td3'>${nameUserInput}</td>
+            <td id='td2'>${typeUserInput}</td>
             <td id='td4'>${amountUserInput}</td>
             <td> 
             <button class='delete' id='deleteButton'>Delete</button>
@@ -31,10 +35,18 @@ function makeTableRow() {
         </tr>
         `;
 
-  tableRef.innerHTML += newRow;
-  inputs.forEach((inputs) => (inputs.value = ""));
-  
-  // const deleteBtn = document.getElementById('deleteButton');
-  tableRef.addEventListener('click', deleteExpense);
-}
+  if (
+    dateUserInput.length > 2 &&
+    typeUserInput.length > 2 &&
+    nameUserInput.length > 2 &&
+    amountUserInput.length >= 2
+  ) {
+    tableRef.innerHTML += newRow;
+    inputs.forEach((inputs) => (inputs.value = ""));
+  } else {
+    window.alert("Please Enter An Expense");
+  }
 
+  tableRef.addEventListener("click", deleteExpense);
+
+}
